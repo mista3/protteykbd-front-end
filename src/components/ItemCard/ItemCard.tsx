740fe0656d79ff3ea328585b265ffbc597550c6a
Maxim Istomin <moistomin@outlook.com>
2022-12-03 18:17:02 +0600

    add checkbox, remove steam card effect

--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
-import { Typography, Button, Card, CardActions, CardMedia, CardContent, IconButton } from '@mui/material';
-import { AddShoppingCartRounded, FavoriteRounded, ShoppingCartCheckoutRounded } from '@mui/icons-material';
-import { SteamCardWrapper } from '@/components';
+import { Typography, Button, Card, CardActions, CardMedia, CardContent, IconButton, Checkbox } from '@mui/material';
+import {
+  AddShoppingCartRounded,
+  FavoriteRounded,
+  RemoveShoppingCartRounded,
+  ShoppingCartCheckoutRounded,
+} from '@mui/icons-material';
 import { ItemEntity } from '@/entities';
 import { ROUTES } from '@/routes';
 
@@ -13,44 +17,60 @@ export const ItemCard = observer(({ image, title, price, sale, id }: ItemEntity)
   const [isLiked, setLiked] = useState(false);
   const [isCart, setCart] = useState(false);
 
+  const location = useLocation();
+
   const nav = useNavigate();
 
   const onClick = () => {
-    nav(`${ROUTES.ITEM}/${id}`)
-  }
+    nav(`${ROUTES.ITEM}/${id}`);
+  };
 
   return (
-    <SteamCardWrapper>
-      <Card className='keyboard-card' elevation={1}>
-        <CardMedia component='img' className='img' image={image} onClick={onClick} />
-        <CardContent>
-          <Typography variant='h6'>{title}</Typography>
-          <Typography component='span' variant='h5'>
-            {price.toLocaleString('ru')} ₽
+    <Card className='keyboard-card' elevation={1}>
+      <CardMedia component='img' className='img' image={image} onClick={onClick} />
+      <CardContent>
+        <Typography variant='h6'>{title}</Typography>
+        <Typography component='span' variant='h5'>
+          {price.toLocaleString('ru')} ₽
+        </Typography>
+        {!!sale && (
+          <Typography component='span' variant='h6' className='sale' color='text.secondary'>
+            {sale.toLocaleString('ru')} ₽
           </Typography>
-          {!!sale && (
-            <Typography component='span' variant='h6' className='sale' color='text.secondary'>
-              {sale.toLocaleString('ru')} ₽
-            </Typography>
-          )}
-        </CardContent>
-        <CardActions className='buttons'>
-          <IconButton size='small' color={isLiked ? 'primary' : 'default'} onClick={() => setLiked(!isLiked)}>
-            <FavoriteRounded />
-          </IconButton>
-          <IconButton
-            size='small'
-            color={isCart ? 'primary' : 'default'}
-            onClick={() => {
-              if (isCart) nav(ROUTES.CART);
-              setCart(true);
-            }}
-          >
-            {isCart ? <ShoppingCartCheckoutRounded /> : <AddShoppingCartRounded />}
-          </IconButton>
-          <Button size='small'>Заказать</Button>
-        </CardActions>
-      </Card>
-    </SteamCardWrapper>
+        )}
+      </CardContent>
+      <CardActions className='buttons'>
+        <IconButton size='small' color={isLiked ? 'primary' : 'default'} onClick={() => setLiked(!isLiked)}>
+          <FavoriteRounded />
+        </IconButton>
+        {location.pathname === ROUTES.CART ? (
+          <>
+            <IconButton
+              size='small'
+              onClick={() => {
+                //remove from cart
+              }}
+            >
+              {<RemoveShoppingCartRounded />}
+            </IconButton>
+            <Checkbox size='small' />
+          </>
+        ) : (
+          <>
+            <IconButton
+              size='small'
+              color={isCart ? 'primary' : 'default'}
+              onClick={() => {
+                if (isCart) nav(ROUTES.CART);
+                setCart(true);
+              }}
+            >
+              {isCart ? <ShoppingCartCheckoutRounded /> : <AddShoppingCartRounded />}
+            </IconButton>
+            <Button size='small'>Заказать</Button>
+          </>
+        )}
+      </CardActions>
+    </Card>
   );
 });
--- src/components/SteamCardWrapper/SteamCardWrapper.tsx
+++ src/components/SteamCardWrapper/SteamCardWrapper.tsx
@@ -7,7 +7,6 @@ export const SteamCardWrapper = ({ children }: { children: ReactNode }) => {
 
   const [rotateX, setRotateX] = useState<number>(0);
   const [rotateY, setRotateY] = useState<number>(0);
-  const [brightness, setBrightness] = useState<number>();
 
   const map = (val: number, minA: number, maxA: number, minB: number, maxB: number) =>
     minB + ((val - minA) * (maxB - minB)) / (maxA - minA);
@@ -16,22 +15,18 @@ export const SteamCardWrapper = ({ children }: { children: ReactNode }) => {
     if (!ref.current) return;
     const localX = e.clientX - ref.current.offsetLeft;
     const localY = e.clientY - ref.current.offsetTop;
-    setRotateX(map(localY, 0, 387, 2, -2));
-    setRotateY(map(localX, 0, 320, -2, 2));
-    setBrightness(map(localY, 0, 387, 1.1, 0.9));
+    setRotateX(map(localY, 0, 387, -2, 2));
+    setRotateY(map(localX, 0, 320, 2, -2));
   };
-  
+
   const onMouseLeave = () => {
     setRotateX(0);
     setRotateY(0);
-    setBrightness(1);  
-  }
+  };
 
   return (
     <div className='steam-card-wrapper' onMouseMove={onMouseMove} onMouseLeave={onMouseLeave} ref={ref}>
-      <div style={{ transform: `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`, filter: `brightness(${brightness})` }}>
-        {children}
-      </div>
+      <div style={{ transform: `rotateX(${rotateX}deg) rotateY(${rotateY}deg)` }}>{children}</div>
     </div>
   );
 };
