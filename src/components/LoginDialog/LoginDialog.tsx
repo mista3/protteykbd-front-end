66697abdd0cf0968842299d08cd856904608e8c5
Ivan_Dimov <66019394+deadly-dead-of-deadman@users.noreply.github.com>
2023-05-22 19:37:22 +0300

    add credentials to auth requests

--- src/components/LoginDialog/LoginDialog.tsx
+++ src/components/LoginDialog/LoginDialog.tsx
@@ -20,6 +20,7 @@ export const LoginDialog = observer(() => {
             fetch( 'http://127.0.0.1:5000/login', {
                 method: 'POST',
                 headers: {"Content-Type": "application/json"},
+                credentials: 'include',
                 body: JSON.stringify(user)
             }).then(
                 res => res.json()
@@ -35,6 +36,7 @@ export const LoginDialog = observer(() => {
             fetch( 'http://127.0.0.1:5000/register', {
                 method: 'POST',
                 headers: {"Content-Type": "application/json"},
+                credentials: 'include',
                 body: JSON.stringify(user)
             }).then(() => {
                 loginStore.toggleSignIn();
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -25,6 +25,7 @@ export const NavBar = observer(() => {
       if (userStore.isSigned){
           fetch( 'http://127.0.0.1:5000/logout', {
               method: 'POST',
+              credentials: 'include',
           }).then(() => {
               userStore.logout();
           })
